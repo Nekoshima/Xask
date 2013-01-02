fb25b7d463de331d2380a33c9105d796a6e3f5ed
hegwin <zwt315@163.com>
2013-01-02 11:07:02 +0800

    initial the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xask::Application.config.secret_token = '7cd412f78823f25d97350b140db0c3e95470205772907af52eb81c6c0e329cf3ef1ca98024dfa931f8b0c0f012015494439744bc56174211e287389669c0981f'
